6fc762bf56c90c1169e3aeaeff38ac63639c827d
Jose Carlos Montero Gómez <sipox11@gmail.com>
2017-10-28 19:51:07 +0200

    Create custom rating bar view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jose Carlos Montero Gómez on 28/10/2017.
+//  Copyright © 2017 sipox11. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
