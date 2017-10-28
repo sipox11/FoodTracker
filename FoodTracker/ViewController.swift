72b82084aa61cb97d25841731bc5420eabc114ec
Jose Carlos Montero Gómez <sipox11@gmail.com>
2017-10-28 18:13:05 +0200

    Implement text field delegate methods to show the meal name in the label after typing it in the text field

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Read the text from the text field and assign display it in the label
+        mealNameLabel.text = textField.text
     }
     
     //MARK: Actions
