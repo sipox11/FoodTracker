9c1653514280c5a84c0882fd0712f53e34a606ca
Jose Carlos Montero Gómez <sipox11@gmail.com>
2017-10-29 11:18:12 +0100

    Add 5 buttons for the rating bar programatically

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
     //MARK: Initialization
     
     override init(frame: CGRect) {
@@ -28,20 +35,21 @@ class RatingControl: UIStackView {
     
     //MARK: Private Methods
     private func setupButtons() {
-        
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+        }
     }
 }
