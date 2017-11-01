33f75aaa6a8cc00828ef5b39ab16930ccaee9f28
Jose Carlos Montero Gómez <sipox11@gmail.com>
2017-11-01 20:20:01 +0100

    Add custom table view cell class and link it to the prototype cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jose Carlos Montero Gómez on 01/11/2017.
+//  Copyright © 2017 sipox11. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
