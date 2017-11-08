90cf7fb75a0d488015bae9f9243249ed51d1864b
Jose Carlos Montero Gómez <sipox11@gmail.com>
2017-11-08 18:43:19 +0100

    Implement data persistance

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -17,6 +17,11 @@ class Meal: NSObject, NSCoding {
     var photo: UIImage?
     var rating: Int
     
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+    
     // MARK: Types
     
     struct PropertyKey {
